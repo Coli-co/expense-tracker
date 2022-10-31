9535df6cb9a8e8d4e2c1a2e5b581b19dbd331941
Coli-co <awestlifeblue@gmail.com>
2022-10-31 12:05:39 +0800

    feat: add category, record, user model

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
-const app = express()
 const PORT = 3000
+
+require('./config/mongoose')
+
+const app = express()
 
 app.get('/', (req, res) => {
   res.send('Hello World')
