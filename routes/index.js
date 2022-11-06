1eba5dffc8cd9123e14ba41ce5bf67b37750afae
Coli-co <awestlifeblue@gmail.com>
2022-11-06 11:14:24 +0800

    feat: add user authentication in records and home routers

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ const home = require('./modules/home')
 const records = require('./modules/records')
 const category = require('./modules/category')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
-router.use('/', home)
-router.use('/records', records)
+router.use('/records', authenticator, records)
 router.use('/category', category)
 router.use('/users', users)
+router.use('/', authenticator, home)
 
 module.exports = router
