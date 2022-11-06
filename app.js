e00ba4e40b9d78a585ec4af550ea9a9df0169a2d
Coli-co <awestlifeblue@gmail.com>
2022-11-06 10:57:37 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const usePassport = require('./config/passport')
 // handlebars helper
 const dateFormat = require('./public/dateHelper')
 const { iconChoose, iconNum } = require('./public/iconHelper')
@@ -26,6 +27,9 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
+
+usePassport(app)
+
 app.use(routes)
 
 app.listen(PORT, () => {
