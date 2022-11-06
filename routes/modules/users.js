1c1a504f3f499c7f79d08a1383fe2c24f74af88f
Coli-co <awestlifeblue@gmail.com>
2022-11-06 14:07:31 +0800

    modified: bcrypt register page password

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -39,24 +40,24 @@ router.post('/register', (req, res) => {
     })
   }
 
-  User.findOne({ email })
-    .then((user) => {
-      if (user) {
-        errors.push({ message: '該信箱已被註冊過了。' })
-        res.render('register', {
-          errors,
-          name,
-          email,
-          password,
-          confirmPassword
-        })
-      } else {
-        return User.create({ name, email, password })
-          .then(() => res.redirect('/'))
-          .catch((err) => console.log(err))
-      }
-    })
-    .catch((err) => connections.log(err))
+  User.findOne({ email }).then((user) => {
+    if (user) {
+      errors.push({ message: '該信箱已被註冊過了。' })
+      res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+  })
+  return bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(password, salt))
+    .then((hash) => User.create({ name, email, password: hash }))
+    .then(() => res.redirect('/'))
+    .catch((err) => console.log(err))
 })
 
 router.get('/logout', (req, res) => {
